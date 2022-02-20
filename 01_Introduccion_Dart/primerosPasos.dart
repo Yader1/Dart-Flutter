8794e34e6c6f869ff25cd35dc7d61ea3e7527260
Yader Campbell <yadermejiac123@gmail.com>
2022-02-20 11:28:39 -0600

    Aprendiendo Dart

---
+++ 01_Introduccion_Dart/primerosPasos.dart
@@ -0,0 +1,32 @@
+void main(){
+	//Variables y constantes
+	var edad = 32;
+	String nombre = "Yader";
+	var name = "Yader";
+
+	final double pi = 3.14;
+
+	//Tipos de datos
+	var number = 12; //Numerico
+	var lastname = "Mejia"; //Cadena de caracteres
+	var mayordeedad = true; //Booleano
+
+	//Operaciones Aritmeticas
+  /*
+    Suma = +
+    Resta = -
+    Multiplicacion = *
+    Division = /
+    Modulo = %
+  */
+
+  //Operaciones Condicionales
+  /**
+   * ==
+   * !=
+   * >
+   * <
+   * >=
+   * =<
+   */
+}
