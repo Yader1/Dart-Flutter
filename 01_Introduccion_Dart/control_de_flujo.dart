3209d70bd5c3d1600804a2ef65dbd7e4dc256c6a
Yader Campbell <yadermejiac123@gmail.com>
2022-02-21 21:58:01 -0600

    if else con dart

--- 01_Introduccion_Dart/control_de_flujo.dart
+++ 01_Introduccion_Dart/control_de_flujo.dart
@@ -1,7 +1,22 @@
 /**
- * Flujos de control{
+ * Flujos de control
  * if Else
  * Switch
  * For Loops
  * While
  */
+
+//if Else
+void main(){
+  bool v1 = true;
+  bool v2 = false;
+
+  if(v1 == v2){
+    print("Si es genial");
+  }else{
+    print("No es genial");
+  }
+
+  //if de manera simplificada
+  String resultados = v1 ? "Si es verdadero" : "Lo contrario";
+}
