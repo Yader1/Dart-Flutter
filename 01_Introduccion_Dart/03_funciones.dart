6a2c983e416cb5650bd410c26890ebc5aaaa0a1e
Yader Campbell <yadermejiac123@gmail.com>
2022-02-23 15:46:34 -0600

    Uso de funciones con parametros opcionales

--- 01_Introduccion_Dart/03_funciones.dart
+++ 01_Introduccion_Dart/03_funciones.dart
@@ -2,6 +2,7 @@ void main(){
   //Invocamos la Funciones
   saludo("Yader");
   saludo3("Onell", "Mejia", 21);
+  saludo4(nombre:"Yader");
 }
 
 //Funciones
@@ -17,10 +18,24 @@ void saludo2(String nombre) => print("Bienvenido $nombre");
  * Funciones con parametros
  * opcionales posicionales
  */
-void saludo3(String nombre, [String apellido, num edad]){
+void saludo3(String nombre, [String apellido = "", num edad = 0]){
   if(apellido != null && edad != null){
     print("Bienvenido $nombre $apellido, edad $edad");
   }else{
     print("Bienvenido $nombre");
   }
+}
+//Parametros opcionales nombrados
+void saludo4({String nombre = "Anonymous", String apellidos = ""}){
+  var saludo4 = new StringBuffer("Bienvenido a Dart");
+
+  //Añadimos los datos al saludo
+  if(nombre != null){
+    saludo4.write("$nombre");
+  }
+  if(apellidos != null){
+    saludo4.write("$apellidos");
+  }
+
+  print(saludo4.toString());
 }
