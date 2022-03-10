b10c7192bd8d860f211643f2a54ff2f0dae6367a
Yader Campbell <yadermejiac123@gmail.com>
2022-03-10 15:10:20 -0600

    Implementando tabs

--- 04_Flutter_Basico/07_tabs/application_tabs/lib/main.dart
+++ 04_Flutter_Basico/07_tabs/application_tabs/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:application_tabs/src/tabs.dart';
 
 void main() {
-  
+  runApp(
+    MaterialApp(
+      home: MyTabs()
+    )
+  );
 }
---
+++ 04_Flutter_Basico/07_tabs/application_tabs/lib/src/tabs.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyTabs extends StatefulWidget {
+  @override
+  _MyTabsState createState() => new _MyTabsState();
+ }
+
+class _MyTabsState extends State<MyTabs> {
+  @override
+  Widget build(BuildContext context) {
+   return 
+   DefaultTabController(
+     //Tamaño de tabs
+     length: 3,
+     child:
+     Scaffold(
+      appBar: AppBar(title:Text("Video Tabs"),
+      bottom: TabBar(tabs: <Widget>[
+        Tab(icon: Icon(Icons.home),),
+        Tab(icon: Icon(Icons.ondemand_video),),
+        Tab(icon: Icon(Icons.contacts,))
+     ],),
+     ),   
+    )
+   );
+  }
+}
