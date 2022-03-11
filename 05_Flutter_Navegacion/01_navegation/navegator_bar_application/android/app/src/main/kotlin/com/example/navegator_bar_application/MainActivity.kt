c3ffd0696b27ce7bf35c143e081f8092b7c8cd21
Yader Campbell <yadermejiac123@gmail.com>
2022-03-10 23:07:03 -0600

    Estructura basica del proyecto

---
+++ 05_Flutter_Navegacion/01_navegation/navegator_bar_application/android/app/src/main/kotlin/com/example/navegator_bar_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navegator_bar_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 05_Flutter_Navegacion/01_navegation/navegator_bar_application/lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget{
+  _app createState() => _app();
+}
+
+class _app extends State<App>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("Primera pantalla"))
+    );
+  }
+}
