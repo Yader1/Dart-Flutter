b3b4aeb3c8e81f8bda624f52453685fe91d36ff6
Yader Campbell <yadermejiac123@gmail.com>
2022-03-14 11:15:07 -0600

    Cambio de pantalla uso de push

--- 05_Flutter_Navegacion/01_navegation/navegator_bar_application/lib/src/app.dart
+++ 05_Flutter_Navegacion/01_navegation/navegator_bar_application/lib/src/app.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:navegator_bar_application/src/screens/segundo.dart';
 
 class App extends StatefulWidget{
   _app createState() => _app();
@@ -8,7 +11,19 @@ class _app extends State<App>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text("Primera pantalla"))
+      appBar: AppBar(title:Text("Primera pantalla")),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Mostrar pantalla"),
+          onPressed: () {
+            //Para navegar a otra pantalla hacemos...
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Segunda())
+            );
+          },
+        )
+      )
     );
   }
 }
---
+++ 05_Flutter_Navegacion/01_navegation/navegator_bar_application/lib/src/screens/segundo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Segunda extends StatefulWidget{
+  _segunda createState() => _segunda();
+}
+
+class _segunda extends State<Segunda>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("Segunda pantalla"))
+    );
+  }
+}
