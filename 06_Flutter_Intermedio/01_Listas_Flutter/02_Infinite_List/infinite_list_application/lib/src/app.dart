b84c0ef476a6d5fb0f37be9af9c199fb022a7008
Yader Campbell <yadermejiac123@gmail.com>
2022-03-17 11:16:26 -0600

    Lista infinita Uso del Builder

--- 06_Flutter_Intermedio/01_Listas_Flutter/02_Infinite_List/infinite_list_application/lib/src/app.dart
+++ 06_Flutter_Intermedio/01_Listas_Flutter/02_Infinite_List/infinite_list_application/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class InfiniteList extends StatefulWidget {
   @override
@@ -9,7 +10,14 @@ class  InfiniteListState extends State <InfiniteList> {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-     appBar: AppBar(title:Text("Infinite List"))
+     appBar: AppBar(title:Text("Infinite List")),
+     body: ListView.builder(itemBuilder: (context, i){
+       return ListTile(
+         leading: Icon(Icons.shop_rounded),
+         title: Text('Producto $i'),
+         subtitle: Text('Precio: ${Random().nextInt(100)} USD'),
+       );
+     }),
    );
   }
 }
