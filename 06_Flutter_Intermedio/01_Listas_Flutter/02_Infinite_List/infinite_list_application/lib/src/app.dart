00b2f61a0511690db5b9b7c7d0cd065f5afa732d
Yader Campbell <yadermejiac123@gmail.com>
2022-03-17 14:42:09 -0600

    Persistencia de datos

--- 06_Flutter_Intermedio/01_Listas_Flutter/02_Infinite_List/infinite_list_application/lib/src/app.dart
+++ 06_Flutter_Intermedio/01_Listas_Flutter/02_Infinite_List/infinite_list_application/lib/src/app.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:english_words/english_words.dart';
 
 class InfiniteList extends StatefulWidget {
   @override
-   InfiniteListState createState() => new  InfiniteListState();
+   InfiniteListState createState() => new InfiniteListState();
  }
 
 class  InfiniteListState extends State <InfiniteList> {
+  final suggestions = <WordPair>[];
+  final saved = <WordPair>[];
+
+  ListTile buildRow(WordPair pair){
+    return ListTile(
+         trailing: Icon(Icons.favorite),
+         title: Text(pair.asPascalCase),
+         onTap: (){
+           setState(() {
+             saved.add(pair);
+           });
+         }
+       );
+  }
   @override
   Widget build(BuildContext context) {
+  //Creamos una nueva page
+  void pushSaved(){
+    Navigator.push(
+      context,
+      MaterialPageRoute( builder: (context){
+        final title = saved.map<ListTile>((pair)=> ListTile(title: Text(pair.asPascalCase),)).toList();
+        return Scaffold(
+          appBar: AppBar(title:Text("Guardados")),
+          body: ListView( children: title),
+        );
+      }
+      )
+    );
+  }
+
    return Scaffold(
-     appBar: AppBar(title:Text("Infinite List")),
+     appBar: AppBar(title:Text("Infinite List"),
+     actions: <Widget>[
+       IconButton(icon: Icon(Icons.list), onPressed: pushSaved)
+     ],),
      body: ListView.builder(itemBuilder: (context, i){
-       return ListTile(
-         leading: Icon(Icons.shop_rounded),
-         title: Text('Producto $i'),
-         subtitle: Text('Precio: ${Random().nextInt(100)} USD'),
-       );
+       if(i.isOdd) return Divider(); //Poner una linea en caso de que sea impar
+
+       if(i >= suggestions.length){
+         suggestions.addAll(generateWordPairs().take(10));
+       }
+
+       return buildRow(suggestions[i]);
      }),
    );
   }
