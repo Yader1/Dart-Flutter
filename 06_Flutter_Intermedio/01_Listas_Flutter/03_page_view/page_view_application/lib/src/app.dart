98c90d2cbf03ae448099d40e47ae6b92e1f55efe
Yader Campbell <yadermejiac123@gmail.com>
2022-03-17 17:12:28 -0600

    Uso de PageView infinitas

--- 06_Flutter_Intermedio/01_Listas_Flutter/03_page_view/page_view_application/lib/src/app.dart
+++ 06_Flutter_Intermedio/01_Listas_Flutter/03_page_view/page_view_application/lib/src/app.dart
@@ -4,13 +4,20 @@ class App extends StatelessWidget {
  @override
  Widget build(BuildContext context) {
   return Container(
+    /* Page con tres paginas
     child: PageView(
       children: <Widget>[
         Container( color: Colors.pink ),
         Container( color: Colors.blueAccent ),
         Container( color: Colors.purpleAccent ),
       ]
-    )
- );
+     );
+    */
+    child: PageView.builder(itemBuilder: (context, i){
+      return Container(
+        color: i % 2 == 0 ? Colors.blueAccent : Colors.redAccent
+      );
+    })
+    );
  }
 }
