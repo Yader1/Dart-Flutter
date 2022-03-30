cfdad44e629b8ad40c50816e76aa9d82247f7b77
Yader Campbell <yadermejiac123@gmail.com>
2022-03-30 08:28:07 -0600

    StreamTrasformer

--- 03_Dart_Avanzado/DART Streams/Streams.dart
+++ 03_Dart_Avanzado/DART Streams/Streams.dart
@@ -2,9 +2,18 @@ import 'dart:async';
 
 void main() {
   final orden = Orden('Tacos');
+  final yader = StreamTransformer.fromHandlers(
+    handleData: (foodType, sink){
+      if(foodType == 'pizza'){
+        sink.add(new Food());
+      }else{
+        sink.addError('No tenemos ese producto');
+      }
+    }
+  );
   final controller = StreamController();
   controller.sink.add(orden);
-  controller.stream.map((orden) => orden.type);
+  controller.stream.map((orden) => orden.type).transform(yader);
 }
 
 class Orden{
