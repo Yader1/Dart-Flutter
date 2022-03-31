7e9d54be8d70fb141e17d3852c8ab7ddd3abfd3e
Yader Campbell <yadermejiac123@gmail.com>
2022-03-31 11:01:05 -0600

    Otro ejemplo de Streams

---
+++ 03_Dart_Avanzado/DART Streams/Streams_02.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+
+void main() {
+  final Element? button = querySelector('button');
+  button?.onClick
+    .timeout(
+      Duration(seconds: 1),
+      onTimeout: (sink) => sink.addError('Perdiste')
+    )
+    .listen(
+      (event) {},
+      onError: (err) => print(err)
+    );
+}
