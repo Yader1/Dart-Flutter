ebb9c879b9434f47fa0275aef9489a82f8ed8c42
Yader Campbell <yadermejiac123@gmail.com>
2022-03-31 11:43:48 -0600

    Streams Input

--- 03_Dart_Avanzado/DART Streams/Streams_02.dart
+++ 03_Dart_Avanzado/DART Streams/Streams_02.dart
@@ -2,6 +2,8 @@ import 'dart:html';
 
 void main() {
   final Element? button = querySelector('button');
+  final Element? input = querySelector('input');
+  /*
   button?.onClick
     .timeout(
       Duration(seconds: 1),
@@ -11,4 +13,11 @@ void main() {
       (event) {},
       onError: (err) => print(err)
     );
+  */
+
+  button?.onClick
+    .take(3)
+    .where((event) => input?.value == "chile")
+    .listen((event) => print("Correcto"),
+    onDone: () => print("no es correcto"));
 }
