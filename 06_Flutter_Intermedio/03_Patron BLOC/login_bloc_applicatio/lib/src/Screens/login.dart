2de1a628c09dee108491b62b5eae07592f727ebf
Yader Campbell <yadermejiac123@gmail.com>
2022-04-23 17:05:42 -0600

    Implementando Widget

--- 06_Flutter_Intermedio/03_Patron BLOC/login_bloc_applicatio/lib/src/Screens/login.dart
+++ 06_Flutter_Intermedio/03_Patron BLOC/login_bloc_applicatio/lib/src/Screens/login.dart
@@ -6,26 +6,38 @@ class LoginScreen extends StatelessWidget {
   return new Container(
     margin: EdgeInsets.all(20.0),
     child: Column(children: <Widget>[
-     // emailField(),
-     // passwordField(),
-     // Container(margin: EdgeInsets.only(top: 25.0)),
-     // submitField()
+     emailField(),
+     passwordField(),
+     Container(margin: EdgeInsets.only(top: 25.0)),
+     submitField()
     ],)
  );
  }
 
-/*
  Widget emailField(){
-
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@email.com',
+        labelText: 'Email',
+        errorText: ''
+      )
+   );
  }
 
  Widget passwordField(){
-
+   return TextField(
+      decoration: InputDecoration(
+        hintText: 'Contraseña',
+        labelText: 'Contraseña'
+      )
+   );
  }
 
  Widget submitField(){
-
+   return RaisedButton( 
+    child: Text('Enviar'),
+    onPressed:(){} 
+  );
  }
-
- */
 }
