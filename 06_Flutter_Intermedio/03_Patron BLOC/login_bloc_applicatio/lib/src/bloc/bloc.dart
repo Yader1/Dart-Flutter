5a9ee19d9216dfb656d6e0ed702658b0d259166d
Yader Campbell <yadermejiac123@gmail.com>
2022-04-26 16:37:40 -0600

    Uso de los trasforms

--- 06_Flutter_Intermedio/03_Patron BLOC/login_bloc_applicatio/lib/src/bloc/bloc.dart
+++ 06_Flutter_Intermedio/03_Patron BLOC/login_bloc_applicatio/lib/src/bloc/bloc.dart
@@ -1,9 +1,18 @@
 import 'dart:async';
+import 'package:login_bloc_applicatio/src/bloc/validators.dart';
 
-class Bloc{
+class Bloc with Validators{
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
 
-  Function(String) get changeEmail => emailController.sink.add;
-  Function(String) get changePassword => passwordController.sink.add;
+  Stream<String> get email => _emailController.stream.transform(validaEmail);
+  Stream<String> get password => _passwordController.stream.transform(validaPassword);
+
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  dispose(){
+    _emailController.close();
+    _passwordController.close();
+  }
 }
