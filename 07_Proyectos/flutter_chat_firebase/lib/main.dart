fad68a06007c144a43f2de61e7daf8a65a743218
Yader Campbell <yadermejiac123@gmail.com>
2022-05-22 01:41:39 -0600

    Definicion y configuracion del proyecto de chat con firebase

---
+++ 07_Proyectos/flutter_chat_firebase/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_firebase/screens/welcome_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Welcome(),
+      //Dejamos definido el tema de la aplicacion
+      theme: ThemeData(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black45),
+           
+        ),
+      )
+    )
+  );
+}
---
+++ 07_Proyectos/flutter_chat_firebase/lib/screens/welcome_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => new _WelcomeState();
+ }
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+   return new Container(
+     child: Text(""),
+   );
+  }
+}
