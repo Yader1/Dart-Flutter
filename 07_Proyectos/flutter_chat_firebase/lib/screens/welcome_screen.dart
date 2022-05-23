4372f4189c7b9f70808fefae6ac88494f756e601
Yader Campbell <yadermejiac123@gmail.com>
2022-05-22 23:40:35 -0600

    Buttons, creacion de botones

--- 07_Proyectos/flutter_chat_firebase/lib/screens/welcome_screen.dart
+++ 07_Proyectos/flutter_chat_firebase/lib/screens/welcome_screen.dart
@@ -5,6 +5,27 @@ class Welcome extends StatefulWidget {
   _WelcomeState createState() => new _WelcomeState();
  }
 class _WelcomeState extends State<Welcome> {
+
+  //Button de base, resive el color del button, el texto que mostrara el button y el evento a realizar
+  Widget getButton(MaterialAccentColor color, String name, VoidCallback onPressed){
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      //Propiedades de estilo del button
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: SizedBox(
+          height: 43.0,
+          child: FlatButton(
+            child: Text(name),
+            onPressed: onPressed,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
    return Scaffold(
@@ -27,7 +48,11 @@ class _WelcomeState extends State<Welcome> {
                   )
               )
             ]
-          )
+          ),
+          SizedBox(height: 45.0,),
+          //Llamamos a nuestro button y enviamos sus especificaciones
+          getButton(Colors.lightBlueAccent, "Lon in", (){}),
+          getButton(Colors.blueAccent, "Registrarse", (){})
         ]
       )
     )
