71be3decfaf93d3f9e42be95cd10683e9c028eca
Yader Campbell <yadermejiac123@gmail.com>
2022-05-23 16:22:21 -0600

    Implementando los widget en login

--- 07_Proyectos/flutter_chat_firebase/lib/src/screens/login_screen.dart
+++ 07_Proyectos/flutter_chat_firebase/lib/src/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_firebase/src/widgets/app_button.dart';
+
+import '../widgets/app_icon.dart';
 
 class login extends StatefulWidget {
   //Definiendo identificador de la Ruta
@@ -10,7 +13,22 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-     appBar: AppBar(title:Text("Login"))
+    body: Container(
+       padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+         //Cntrar y ocupar todo el ancho de la pantalla
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+
+        children: <Widget>[
+          //Llamamos nuestro widget del titulo de la aplicacion.
+          AppIcon(),
+          SizedBox(height: 45.0,),
+          //Llamamos a nuestro button y enviamos sus especificaciones
+          AppButton(color: Colors.blueAccent, onPressed: (){ }, name: "Lon in")
+        ]
+      )
+    )
    );
   }
 }
