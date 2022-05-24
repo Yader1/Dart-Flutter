c85584c1a070fe64aa45e23d4ef7b88d086de134
Yader Campbell <yadermejiac123@gmail.com>
2022-05-23 22:22:22 -0600

    Text Field, del login

--- 07_Proyectos/flutter_chat_firebase/lib/src/screens/login_screen.dart
+++ 07_Proyectos/flutter_chat_firebase/lib/src/screens/login_screen.dart
@@ -10,6 +10,25 @@ class login extends StatefulWidget {
   _loginState createState() => new _loginState();
  }
 class _loginState extends State<login> {
+  //Widget de campo de datos
+  TextField getTextField(String inputText){
+    return TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        hintText: inputText,
+        //Diseño circular del campo de texto
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(32.0))
+        ),
+        //Diseño de color del border del campo de texto
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+          borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2),
+        ),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
    return Scaffold(
@@ -23,7 +42,11 @@ class _loginState extends State<login> {
         children: <Widget>[
           //Llamamos nuestro widget del titulo de la aplicacion.
           AppIcon(),
-          SizedBox(height: 45.0,),
+          SizedBox(height: 48.0,),
+          getTextField("Ingresar Email"),
+          SizedBox(height: 8.0,),
+          getTextField("Ingresar contraseña"),
+          SizedBox(height: 23.0,),
           //Llamamos a nuestro button y enviamos sus especificaciones
           AppButton(color: Colors.blueAccent, onPressed: (){ }, name: "Lon in")
         ]
