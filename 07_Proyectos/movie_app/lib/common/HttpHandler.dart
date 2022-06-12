555a9660dc9724b8001380bade25523be3fdd63f
Yader Campbell <yadermejiac123@gmail.com>
2022-06-11 22:23:26 -0600

    Creacion de clases hijas y implementacion de fetch show

--- 07_Proyectos/movie_app/lib/common/HttpHandler.dart
+++ 07_Proyectos/movie_app/lib/common/HttpHandler.dart
@@ -10,10 +10,17 @@ import 'package:movie_app/common/Constant.dart';
 import 'package:movie_app/model/Media.dart';
 
 class HttpHandler{
+  //Metodo de inicializacion
+  static final _httpHandler = new HttpHandler();
+
   //Definimos propiedades relacionada al URL
   final String _baseUrl = "api.themoviedb.org";
   final String _lenguaje = "es-ES";
 
+  //Metodo encargado de retornar el obj.
+  static HttpHandler get(){
+    return _httpHandler;
+  }
 
   //Metodo principal
   Future<dynamic> getJson(Uri uri) async{
@@ -24,6 +31,20 @@ class HttpHandler{
   //Llamada a los datos
   Future<List<Media>> fechMovie(){
     var uri = new Uri.http(_baseUrl, "3/movie/popular", {
+      'api_key' : API_KEY,
+      'page' : "1",
+      'language' : _lenguaje
+    });
+
+    return getJson(uri).then(((data) =>
+      //Accedemos a los datos segun la estructura del Json a mapiar
+      data['results'].map<Media>((item) => Media(item)).toList()
+    ));
+  }
+
+  //Lamamos get TV
+  Future<List<Media>> fechShow(){
+    var uri = new Uri.http(_baseUrl, "3/tv/popular", {
       'api_key' : API_KEY,
       'page' : "1",
       'language' : _lenguaje
--- 07_Proyectos/movie_app/lib/common/MediaProvider.dart
+++ 07_Proyectos/movie_app/lib/common/MediaProvider.dart
@@ -1,6 +1,23 @@
 import 'dart:async';
 import 'package:movie_app/model/Media.dart';
+import 'package:movie_app/common/HttpHandler.dart';
 //Se utilizara para definir los proveedores
 abstract class MediaProvider {
   Future<List<Media>> fechMedia();
+}
+
+class MovieProvider extends MediaProvider{
+  HttpHandler _client = HttpHandler.get();
+  @override
+  Future<List<Media>> fechMedia() {
+    return _client.fechMovie();
+  }
+}
+
+class ShowProvider extends MediaProvider{
+  HttpHandler _client = HttpHandler.get();
+  @override
+  Future<List<Media>> fechMedia() {
+    return _client.fechShow();
+  }
 }
