f8444066b1d8586d119790a80f28d24950243536
Yader Campbell <yadermejiac123@gmail.com>
2022-06-24 17:50:57 -0600

    Mapiar errores de firebase

---
+++ 07_Proyectos/flutter_chat_firebase/lib/src/model/auth_request.dart
@@ -0,0 +1,5 @@
+//Se utilizara para mostrar los errores de login o registro de firebase
+class AuthenticationRequest {
+  bool success = false;
+  late String errorMessage;
+}
--- 07_Proyectos/flutter_chat_firebase/lib/src/services/autentication.dart
+++ 07_Proyectos/flutter_chat_firebase/lib/src/services/autentication.dart
@@ -1,50 +1,89 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_chat_firebase/src/model/auth_request.dart';
 
-class Autentication{
-   //Instanciamos el servicio Auth
+class Autentication {
+  //Instanciamos el servicio Auth
   final _auth = FirebaseAuth.instance;
-  Future<UserCredential?> createUser({String email="", String password=""}) async{
-    try{
-    //Enviamos el email y el password a firebase Auth
-      return await _auth.createUserWithEmailAndPassword(email: email, password: password);  
-    }catch(e){
-      print(e);
-    } 
+  Future<AuthenticationRequest?> createUser(
+      {String email = "", String password = ""}) async {
+    AuthenticationRequest authRequest = AuthenticationRequest();
+    try {
+      //Enviamos el email y el password a firebase Auth
+      var user = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      if (user != null) {
+        authRequest.success = true;
+      }
+    } catch (e) {
+      _mapErrorMessage(authRequest, e.hashCode.toString());
+    }
 
-    return null; 
+    return authRequest;
   }
 
-  Future<User?> getCurrentUser() async{
-    try{
-    //Enviamos el email y el password a firebase Auth
-      return await _auth.currentUser!;  
-    }catch(e){
+  Future<User?> getCurrentUser() async {
+    try {
+      //Enviamos el email y el password a firebase Auth
+      return await _auth.currentUser!;
+    } catch (e) {
       print(e);
-    } 
+    }
 
-    return null; 
+    return null;
   }
 
   //Login user
-  Future<UserCredential?> loginUser({String email="", String password=""}) async{
-    try{
-    //Enviamos el email y el password a firebase Auth
-      return await _auth.signInWithEmailAndPassword(email: email, password: password);  
-    }catch(e){
-      print(e);
-    } 
+  Future<AuthenticationRequest> loginUser(
+      {String email = "", String password = ""}) async {
+    AuthenticationRequest authRequest = AuthenticationRequest();
+    try {
+      //Enviamos el email y el password a firebase Auth
+      var user = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      if (user != null) {
+        authRequest.success = true;
+      }
+    } catch (e) {
+      _mapErrorMessage(authRequest, e.hashCode.toString());
+    }
 
-    return null; 
+    return authRequest;
   }
 
   //Cerrar sesion
-  Future<void> singOut() async{
-    try{
-      return await _auth.signOut(); 
-    }catch(e){
+  Future<void> singOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
       print(e);
-    } 
+    }
 
-    return null; 
+    return null;
+  }
+
+  //Metodo para mapiar los errores de firebase
+  void _mapErrorMessage(
+      AuthenticationRequest authenticationRequest, String code) {
+    switch (code) {
+      case 'ERROR_USER_NOT_FOUND':
+        authenticationRequest.errorMessage = "Usuario no encontrado";
+        break;
+
+      case 'ERROR_WRONG_PASSWORD':
+        authenticationRequest.errorMessage = "Contraseña invalida";
+        break;
+
+      case 'ERROR_EMAIL_ALREADY_IN_USE':
+        authenticationRequest.errorMessage = "El usuario ya esta registrado";
+        break;
+
+      case 'ERROR_NETWORK_REQUEST_FAILED':
+        authenticationRequest.errorMessage = "Error de red";
+        break;
+
+      default:
+        authenticationRequest.errorMessage = code;
+        break;
+    }
   }
 }
