48c95c55a75243674bbe2e88bc3cea67510cf0af
Yader Campbell <yadermejiac123@gmail.com>
2022-06-25 07:38:00 -0600

    Pantalla de chat diseño

--- 07_Proyectos/flutter_chat_firebase/lib/src/screens/chat_screen.dart
+++ 07_Proyectos/flutter_chat_firebase/lib/src/screens/chat_screen.dart
@@ -7,7 +7,8 @@ class ChatScreen extends StatefulWidget {
   static const String routeName = '/chat';
   @override
   _ChatScreenState createState() => new _ChatScreenState();
- }
+}
+
 class _ChatScreenState extends State<ChatScreen> {
   //Inicializamos de manera inmediada
   @override
@@ -16,30 +17,71 @@ class _ChatScreenState extends State<ChatScreen> {
     getCurrentUser();
   }
 
+  BoxDecoration _messageContainerDecoration = BoxDecoration(
+      border: Border(
+    top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+  ));
+
+  InputDecoration _messageTextFieldDecoration = InputDecoration(
+      contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      hintText: 'Ingresar su mensaje aqui...',
+      border: InputBorder.none);
+
+  TextStyle _sendButtonStyle = TextStyle(
+    color: Colors.lightBlueAccent,
+    fontWeight: FontWeight.bold,
+    fontSize: 18.0
+  );
+
   //Optener datos del usuario de firebase
   final auth = FirebaseAuth.instance;
   late User loggedInUser;
   Future<void> getCurrentUser() async {
-      var user = await Autentication().getCurrentUser();
-      if(user != null ){
-        loggedInUser = user;
-        print(loggedInUser.email);
-      }
+    var user = await Autentication().getCurrentUser();
+    if (user != null) {
+      loggedInUser = user;
+      print(loggedInUser.email);
+    }
   }
+
   @override
   Widget build(BuildContext context) {
-   return new Scaffold(
-     appBar: AppBar(title:Text("Chat Flutter"),
-     actions: <Widget>[
-      //Button para des logger
-      IconButton(
-        icon: Icon(Icons.power_settings_new),
-        onPressed: (){
-          Autentication().singOut();
-          Navigator.pop(context);
-        }
-      )
-     ],),
-   );
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Chat Flutter"),
+        actions: <Widget>[
+          //Button para des logger
+          IconButton(
+              icon: Icon(Icons.power_settings_new),
+              onPressed: () {
+                Autentication().singOut();
+                Navigator.pop(context);
+              })
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: _messageContainerDecoration,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: TextField(
+                    decoration: _messageTextFieldDecoration,
+                  )),
+                  FlatButton(
+                    onPressed: () {
+                      
+                    },
+                    child: Text("Enviar", style: _sendButtonStyle),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
